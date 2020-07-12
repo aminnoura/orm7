3104b409dd6ef1b4bdce67cf7a76d4cd651a628e
aminnoura <aminnoora1@yahoo.com>
2020-07-12 15:02:04 +0200

    add a sample ORM and get ready to build some tasks for ORM

---
+++ orm/user.php
@@ -0,0 +1,12 @@
+<?php
+namespace orm;
+class User extends dbTasks{
+    private $id;
+    private $firstname;
+    private $lastname;
+    private $age;
+    private $gender;
+    private $role;
+    private $location;
+    private $note;
+}
