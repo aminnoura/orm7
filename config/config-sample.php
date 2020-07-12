4dfc1cd31ccb532342ef1a8ef2656b3421791248
aminnoura <aminnoora1@yahoo.com>
2020-07-12 21:03:20 +0200

    add a sample config file

---
+++ config/config-sample.php
@@ -0,0 +1,25 @@
+<?php
+
+// // uncomment first and rename to config.php
+
+//namespace config;
+//ini_set ( 'display_errors', 1);
+//
+//class Config {
+//    /**
+//     * @var string[]
+//     */
+//    private $databaseInfo = [
+//        'host' => '',
+//        'database' => '',
+//        'username' => '',
+//        'password' => '',
+//    ];
+//
+//    /**
+//     * @return string[]
+//     */
+//    public function getDatabaseInfo(){
+//        return $this->databaseInfo;
+//    }
+//}
